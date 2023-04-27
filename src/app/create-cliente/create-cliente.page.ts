9e483b0d9b09b730c50bc84039ab5d2a536656a5
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2023-04-27 20:37:35 -0300

    criando forms
    criando forms

--- src/app/create-cliente/create-cliente.page.ts
+++ src/app/create-cliente/create-cliente.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-cliente',
@@ -12,7 +13,12 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CreateClientePage implements OnInit {
 
-  constructor() { }
+  nome = '';
+  email = '';
+  senha = '';
+  confirmarSenha = '';
+
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Cliente } from '../models/Cliente.model';
 import { ClientesService } from '../services/clientes.service';
+import { RouterLink } from 'angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, RouterLink],
 })
 export class HomePage {
 
